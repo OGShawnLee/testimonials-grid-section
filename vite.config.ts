e5be7ff50d31ae509eb84e349e509681fc91edfc
Shawn Lee <changoloco.evg@gmail.com>
2022-05-20 22:05:19 -0500

    chore(alias): :wrench: add components path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import windicss from 'vite-plugin-windicss';
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte(), windicss()],
+  resolve: {
+    alias: {
+      '@components': resolve('src/components'),
+      '@components/*': resolve('src/components/*'),
+    },
+  },
 });
